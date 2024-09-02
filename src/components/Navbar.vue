46fc5dd12c563870a4167a7190638ac9f91ca28b
Damilola Akinlolu <confinesofgrace@gmail.com>
2024-09-02 09:43:30 +0100

    Hamburger Created

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -6,19 +6,26 @@
 
         <!-- Padding of 40px for the Nav-->
 
-        <div id="nav-logo"> <RouterLink to="/"> <h2>Tabernacle of David</h2> </RouterLink> </div>
+        <div id="nav-logo"> 
+            <RouterLink to="/"> <h2>Tabernacle of David</h2> </RouterLink> 
+            
+            <div id="hamburger"> 
+                <hr id="hr-1">
+                <hr id="hr-2">
+                <hr id="hr-3">
+            </div>
+        </div>
         
         <div id="nav-center"> 
 
             <RouterLink to="/about" ><div id="navs">About</div></RouterLink> 
             <RouterLink to="/our-lyrics"> <div id="navs">Our Lyrics</div> </RouterLink>
             <RouterLink to="/our-collection"> <div id="navs"> Our Collection</div></RouterLink>
-            <RouterLink to="/write-lyrics"> <div id="navs"> Write Lyrics</div></RouterLink>
-            
+            <RouterLink to="/write-lyrics"> <div id="navs"> Write Lyrics</div></RouterLink>    
         
         </div>
 
-        <div id="hamburger"> Menu </div>
+        
         
 
     </nav>
@@ -79,7 +86,33 @@ nav
 
     #hamburger
     {
+        display: block;
+        padding: 10px ;
+    }
+
+    hr
+    {
+        width: 20px;
+    }
+
+    /*
+    #hr-1
+    {
+        width: 15px;
+    }
+    */
+
+    #hr-2
+    {
+        margin: 4px 0px;
+    }
+
+    #nav-logo
+    {
+        width: 100%;
         display: flex;
+        justify-content: space-around;
+        align-items: center;
     }
 }
 </style>
